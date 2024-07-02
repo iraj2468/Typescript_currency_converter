c8668e50ae7a3ac6064288b1b8d887771a72191a
iraj2468 <irajrashid2468@gmail.com>
2024-07-02 13:04:09 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1, //BASE CURRRENCY
+    EUR: 0.94,
+    GBP: 0.43,
+    IND: 74.90,
+    PKR: 280
+};
+let answers = await inquirer.prompt([{
+        name: "from",
+        message: "Enter from currency",
+        type: "list",
+        choices: [`USD`, `EUR`, `GBP`, `IND`, `PKR`]
+    }, {
+        name: "to",
+        message: "Enter to currency",
+        type: "list",
+        choices: [`USD`, `EUR`, `GBP`, `IND`, `PKR`]
+    }, {
+        name: "amount",
+        message: "Enter your amount",
+        type: "number"
+    }
+]);
+let fromamount = currency[answers.from];
+let toamount = currency[answers.to];
+let amount = answers.amount;
+let baseAmount = amount / fromamount;
+let convertedAmount = baseAmount * toamount;
+console.log(convertedAmount);
